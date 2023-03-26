3547f008ed395134d3406e145762f3d25c026ddb
linenive <linenive@naver.com>
2023-03-26 11:45:49 +0900

    express 임포트

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello Janjan!");
+import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
+
+const app: Application = express();
+const port = 3000;
+
+app.use(morgan('dev'));
+
+app.get('/hello', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
